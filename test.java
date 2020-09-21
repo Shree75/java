56845fa9b7eb10bb18b13e956ad0e270c4cfe7e9
Shree75 <shreenathkadam75@gmail.com>
2020-09-21 10:00:09 +0530

    committed

---
+++ test.java
@@ -0,0 +1,80 @@
+root@75:~# nano
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  GNU nano 2.0.6                                     New Buffer                                                                      Modified  
+
+public class test
+{
+        public static void main(String[] args)
+        {
+                System.out.println("HEllo World");
+        }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+^G Get Help            ^O WriteOut            ^R Read File           ^Y Prev Page           ^K Cut Text            ^C Cur Pos
+^X Exit                ^J Justify             ^W Where Is            ^V Next Page           ^U UnCut Text          ^T To Spell
